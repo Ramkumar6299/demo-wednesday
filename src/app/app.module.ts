de0676f1e1ce8d7f0ca46bd408c6d58bf51c3ba1
Krishnamoorthy <RAMKUMAR.KRISHNAMOORTHY@Chubb.com>
2020-07-08 20:09:52 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AccordionComponent } from './accordion/accordion.component';
+import { ModelComponent } from './model/model.component';
+import { CarouselComponent } from './carousel/carousel.component';
+import { TypeheadComponent } from './typehead/typehead.component';
+import { PopoverComponent } from './popover/popover.component';
+import {FormsModule} from '@angular/forms'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AccordionComponent,
+    ModelComponent,
+    CarouselComponent,
+    TypeheadComponent,
+    PopoverComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
